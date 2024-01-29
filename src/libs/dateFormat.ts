0bafc79c30f4bb76fb3d1695202c2a3522809eed
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2024-01-29 10:53:17 -0300

    feat: formatando datas no componente de pedidos

---
+++ src/libs/dateFormat.ts
@@ -0,0 +1,7 @@
+export const dateFormat = (date: string | Date) => {
+    return new
+        Intl.DateTimeFormat(
+            'pt-BR',
+            {dateStyle: 'short', timeStyle: 'short'}
+    ).format(date instanceof  Date ?  date : new Date(date))
+}
